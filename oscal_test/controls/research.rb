48be2e5aed30b58b65dee00efc74a697b393aa36
dameng101 <96740961+dameng101@users.noreply.github.com>
2020-11-16 18:02:45 +0000

    tpyo

--- oscal_test/controls/research.rb
+++ oscal_test/controls/research.rb
@@ -5,3 +5,7 @@ title "sample section"
 control "rsch-1" do 
   impact 0.7 
   title "Connect to Research Source" 
+  describe host(input('website_target'), port: 80, protocol: 'tcp') do
+    it { should be_reachable }
+  end
+end
